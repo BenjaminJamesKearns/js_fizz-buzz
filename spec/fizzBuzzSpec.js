5da807d49c04a0eb477847fab727b48545802aa7
Benjamin Kearns <benjaminkearns@Benjamins-MacBook-Pro.local>
2018-11-06 13:43:59 +0000

    added passing tests for 3,5 and 15

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,12 +1,23 @@
 describe('Javabuzz', function() {
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   var javabuzz;
 
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('divisible by 3 and 5', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
   });
@@ -14,8 +25,15 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+    });
+
+    it('divisible by 3 and 5', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toBe(false);
     });
 
   });
